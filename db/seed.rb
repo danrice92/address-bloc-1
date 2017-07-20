411a78481e8111403b73f9f50422bc7bfbb284b4
Dan Rice <dan.rice.92@outlook.com>
2017-07-20 14:42:01 -0600

    Databases Assignment 6 completed

--- db/seed.rb
+++ db/seed.rb
@@ -11,20 +11,14 @@ book = AddressBook.create(name: 'My Address Book')
 # Entry.create(address_book_id: book.id, name: 'Foo One', phone_number: '999-999-9999', email: 'foo_one@gmail.com' )
 # Entry.create(address_book_id: book.id, name: 'Foo Two', phone_number: '111-111-1111', email: 'foo_two@gmail.com' )
 # Entry.create(address_book_id: book.id, name: 'Foo Three', phone_number: '222-222-2222', email: 'foo_three@gmail.com' )
-# Entry.create(address_book_id: book.id, name: "Dave", phone_number: '444-444-4444', email: "davyboy@gmail.com")
+# Entry.create(address_book_id: book.id, name: "Bill", phone_number: '444-444-4444', email: "bill@gmail.com")
 # all_the_updates = { 1 => { "name" => "Dave", "email" => "david@bloc.com", "phone_number" => "111-111-1111" }, 2 => { "name" => "Jeremy", "email" => "jeremy@bloc.com", "phone_number" => "222-222-2222" }, 3 => { "name" => "Juan", "email" => "juan@bloc.com", "phone_number" => "333-333-3333" } }
-
+#
 # Entry.update(all_the_updates.keys, all_the_updates.values)
 
+# Entry.destroy_all(["phone_number = ?", '111-111-1111'])
 
-# fellow = Entry.first
-#
-# fellow.update_name("Dave")
-#
-# puts Entry.all
 
-fellows = Entry.all
+Entry.where(name: "Juan").destroy_all
 
-puts fellows.where(name: "Dave")
-
-puts fellows.not(name: "Dave")
+puts Entry.all
